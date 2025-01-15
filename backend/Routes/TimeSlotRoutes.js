e0820942f00a5e42c888f5d76a327669788669c7
OmChauhan16 <omc5246@gmail.com>
2025-01-16 02:03:47 +0530

    Hr-Interview Scheduing done

--- backend/Routes/TimeSlotRoutes.js
+++ backend/Routes/TimeSlotRoutes.js
@@ -1,12 +1,10 @@
 import express from "express";
 import { getTimeSlots, addTimeSlot, deleteTimeSlot } from "../controllers/TimeSlotController.js";
-import { loginHR } from "../Controllers/LoginController.js";
 const router = express.Router();
 
 // Routes for time slots
 router.get("/timeslots", getTimeSlots);
 router.post("/timeslots", addTimeSlot);
 router.delete("/timeslots/:id", deleteTimeSlot);
-router.delete("/login", loginHR);
 
 export default router;
